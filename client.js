063f8f6c06bf4bf47a546d668bdbca38a737a657
Unknown <alexandr.fedoseev@aol.com>
2018-03-15 01:36:48 +0300

    adds currency converter app

---
+++ client.js
@@ -0,0 +1,48 @@
+"use strict";
+
+if (process.argv == null || process.argv.length != 4) {
+  console.log('Two arguments is required as {amount: number} \'{from:currency}->{to:currency}\'');
+  return;
+}
+if (isNaN(process.argv[2])) {
+  console.log('Amount should be number');
+  return;
+}
+const amount = process.argv[2];
+const currencies = process.argv[3].split('->');
+if (currencies.length != 2) {
+  console.log('Convertion options should be \'{from:currency}->{to:currency}\'');
+  return;
+}
+if (currencies.some(item => item.length != 3)) {
+  console.log('Currencies should be 3 letters long');
+  return;
+}
+
+const thrift = require('thrift');
+const CurrencyConverter = require('./gen-nodejs/CurrencyConverter');
+const ttypes = require('./gen-nodejs/currency-converter_types');
+const assert = require('assert');
+
+const transport = thrift.TBufferedTransport;
+const protocol = thrift.TBinaryProtocol;
+
+const connection = thrift.createConnection("localhost", 9090, {
+  transport : transport,
+  protocol : protocol
+});
+
+connection.on('error', function(err) {
+  assert(false, err);
+});
+
+const client = thrift.createClient(CurrencyConverter, connection);
+
+client.convert(amount, currencies[0], currencies[1], (err, result) => {
+  if (err) {
+    console.log("Error : " + err);
+  } else {
+    console.log(result);
+  }
+  connection.end();
+});
---
+++ server.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const thrift = require("thrift");
+const request = require('request');
+const CurrencyConverter = require("./gen-nodejs/CurrencyConverter");
+const ttypes = require("./gen-nodejs/currency-converter_types");
+
+const API_ACCESS_KEY = 'NXbSN8gcTSbWJkNJ2JtqbrFGRqM3P5';
+const API_URL_BASE = 'https://www.amdoren.com/api/currency.php';
+
+function calculateRate(base, converted) {
+    return converted / base;
+}
+
+const server = thrift.createServer(CurrencyConverter, {
+    convert: (amount, fromCurrency, toCurrency, result) => {
+        request(`${
+            API_URL_BASE
+        }?api_key=${
+            API_ACCESS_KEY
+        }&from=${
+            fromCurrency.toUpperCase()
+        }&to=${
+            toCurrency.toUpperCase()
+        }&amount=${
+            amount
+        }`, (error, response, body) => {
+            if (error != null || response.statusCode != 200) {
+                const err = new ttypes.RequestError();
+                err.message = 'Currency API is broken';
+                result(err);
+                return;
+            }
+            const parsedBody = JSON.parse(body);
+            if (parsedBody.error != 0) {
+                const err = new ttypes.RequestError();
+                err.message = parsedBody.error_message;
+                result(err);
+                return;
+            }
+            const res = `${amount} ${fromCurrency} = ${
+                parsedBody.amount.toFixed(2)} ${
+                toCurrency} (1 ${fromCurrency} = ${
+                calculateRate(amount, parsedBody.amount).toFixed(2)} ${
+                toCurrency})`;
+            result(null, res);
+        });
+    },
+});
+
+server.listen(9090);
