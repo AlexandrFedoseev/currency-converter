ea106a9962103db587fa9444ab21f64feea03ba9
AlexandrFedoseev <alexandr.fedoseev@aol.com>
2018-03-15 12:12:02 +0300

    connects client to digital ocean

--- client.js
+++ client.js
@@ -1,50 +1,49 @@
-'use strict';
+'use-strict';
 
-if (process.argv == null || process.argv.length != 4) {
-  console.log('Two arguments is required as {amount: number} \'{from:currency}->{to:currency}\'');
-  return;
-}
-if (isNaN(process.argv[2])) {
-  console.log('Amount should be number');
-  return;
-}
-const amount = process.argv[2];
-const currencies = process.argv[3].split('->');
-if (currencies.length != 2) {
-  console.log('Convertion options should be \'{from:currency}->{to:currency}\'');
-  return;
-}
-if (currencies.some(item => item.length != 3)) {
-  console.log('Currencies should be 3 letters long');
-  return;
-}
+(function(process, console){
+    if (process.argv == null || process.argv.length != 4) {
+        console.log('Two arguments is required as {amount: number} \'{from:currency}->{to:currency}\'');
+        return;
+    }
+    if (isNaN(process.argv[2])) {
+        console.log('Amount should be number');
+        return;
+    }
+    var amount = process.argv[2];
+    var currencies = process.argv[3].split('->');
+    if (currencies.length != 2) {
+        console.log('Convertion options should be \'{from:currency}->{to:currency}\'');
+        return;
+    }
+    if (currencies.some(item => item.length != 3)) {
+        console.log('Currencies should be 3 letters long');
+        return;
+    }
 
-const thrift = require('thrift');
-const CurrencyConverter = require('./gen-nodejs/CurrencyConverter');
-const ttypes = require('./gen-nodejs/currency-converter_types');
-const assert = require('assert');
+    var thrift = require('thrift');
+    var CurrencyConverter = require('./gen-nodejs/CurrencyConverter');
+    var ttypes = require('./gen-nodejs/currency-converter_types');
+    var assert = require('assert');
+    const transport = thrift.TBufferedTransport;
+    const protocol = thrift.TBinaryProtocol;
 
-const transport = thrift.TBufferedTransport;
-const protocol = thrift.TBinaryProtocol;
+    const connection = thrift.createConnection('138.68.191.55', 5000, {
+        transport: transport,
+        protocol: protocol
+    });
 
-const connection = thrift.createConnection('https://www.currency-converter-thr.herokuapp.com', 80, {
-  transport : transport,
-  protocol : protocol
-});
-
-connection.on('error', function(err) {
-  assert(false, err);
-});
-
-const client = thrift.createClient(CurrencyConverter, connection);
-client.ping(function(err, response) {
-  console.log('ping()');
-});
-client.convert(amount, currencies[0], currencies[1], (err, result) => {
-  if (err) {
-    console.log('Error : ' + err);
-  } else {
-    console.log(result);
-  }
-  connection.end();
-});
+    connection.on('error', function (err) {
+        assert(false, err);
+    });
+    console.log('connected to http://138.68.191.55:5000');
+    const client = thrift.createClient(CurrencyConverter, connection);
+    client.ping(function (err, response) {});
+    client.convert(amount, currencies[0], currencies[1], (err, result) => {
+        if (err) {
+            console.log('Error : ' + err);
+        } else {
+            console.log(result);
+        }
+        connection.end();
+    });
+})(process, console);
